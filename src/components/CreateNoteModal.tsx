6351f1a137cee63a06b9bf8facf1fc7f642ab63b
Vitalii Korol <korolvitalik@gmail.com>
2021-07-29 11:34:29 +0200

    fix form

--- src/components/CreateNoteModal.tsx
+++ src/components/CreateNoteModal.tsx
@@ -19,7 +19,6 @@ type FormData = {
 
 const CreateNoteModal = (props: ModalProps) => {
   const { showModal, onClickCloseForm } = props;
-
   const {
     register,
     setValue,
@@ -27,7 +26,10 @@ const CreateNoteModal = (props: ModalProps) => {
     formState: { errors },
   } = useForm<FormData>();
 
-  const onSubmit = handleSubmit((data) => console.log(data));
+  const onSubmit = handleSubmit((data) => {
+    onClickCloseForm();
+    console.log(data);
+  });
 
   return (
     <Modal show={showModal} onHide={onClickCloseForm} animation={false}>
@@ -36,53 +38,39 @@ const CreateNoteModal = (props: ModalProps) => {
       </Modal.Header>
       <Modal.Body>
         <Form onSubmit={onSubmit}>
-          <Form.Group as={Row} className='mb-3' controlId='formHorizontalEmail'>
-            <Form.Label column sm={2}>
-              Name
-            </Form.Label>
-            <Col sm={10}>
-              <Form.Control type='Name' placeholder='Name' {...register('name')} />
-            </Col>
+          <Form.Group className='mb-3'>
+            <Form.Label>Name</Form.Label>
+            <Form.Control type='name' placeholder='Enter name' {...register('name')} />
           </Form.Group>
 
-          <Form.Group as={Row} className='mb-3' controlId='formHorizontalPassword'>
-            <Form.Label column sm={2}>
-              Dates
-            </Form.Label>
-            <Col sm={10}>
-              <Form.Control type='Dates' placeholder='Dates' {...register('dates')} />
-            </Col>
+          <Form.Group className='mb-3'>
+            <Form.Label>Content</Form.Label>
+            <Form.Control type='name' placeholder='Add content' {...register('content')} />
           </Form.Group>
 
-          <Form.Group as={Row} className='mb-3' controlId='formHorizontalPassword'>
-            <Form.Label column sm={2}>
-              Content
-            </Form.Label>
-            <Col>
-              <Form.Control type='Content' placeholder='Content' {...register('content')} />
-            </Col>
+          <Form.Group className='mb-3'>
+            <Form.Label>Dates</Form.Label>
+            <Form.Control type='name' placeholder='Add dates' {...register('dates')} />
           </Form.Group>
 
-          <Form.Label column sm={10}>
-            Category
-            <Form.Select aria-label='Default select example'>
-              <option>Choose category</option>
-              <option value='1'>Task</option>
-              <option value='2'>Idea</option>
-              <option value='3'>Quote</option>
-              <option value='4'>Random Thougth</option>
-            </Form.Select>
-          </Form.Label>
+          <Form.Group>
+            <Form.Label column sm={10}>
+              Category
+              <Form.Select aria-label='choose category' {...register('category')}>
+                <option>Choose category</option>
+                <option value='Task'>Task</option>
+                <option value='Idea'>Idea</option>
+                <option value='Quote'>Quote</option>
+                <option value='Random Thougth'>Random Thougth</option>
+              </Form.Select>
+            </Form.Label>
+          </Form.Group>
+          <Button variant='primary' type='submit'>
+            Submit
+          </Button>
         </Form>
       </Modal.Body>
-      <Modal.Footer>
-        <Button variant='secondary' onClick={onClickCloseForm}>
-          Close
-        </Button>
-        <Button variant='primary' type='submit'>
-          Add note
-        </Button>
-      </Modal.Footer>
+      <Modal.Footer></Modal.Footer>
     </Modal>
   );
 };
